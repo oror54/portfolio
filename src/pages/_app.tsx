711de69719675bce416fa2ced0fb07c25153968e
jihyeon <jihyeon33@gmail.com>
2023-06-23 01:34:36 +0900

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="user-scalable=no,width=device-width,initial-scale=1"
+        />
+
+        <title>jihyeon portfolio</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import Head from "next/head";
+import "@/styles/page.module.scss";
+
+export default function index() {
+  return <>hello</>;
+}
+
+index.defaultProps = {};
