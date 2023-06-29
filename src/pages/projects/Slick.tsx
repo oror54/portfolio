b78de593a6da7640b653cd5006a960e6a7117c02
jihyeon <jihyeon33@gmail.com>
2023-06-29 18:11:52 +0900

    slick 수정

--- src/pages/projects/Slick.tsx
+++ src/pages/projects/Slick.tsx
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { useMemo } from "react";
 import Slider from "react-slick";
-import "~slick-carousel/slick/slick.css";
-import "~slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
