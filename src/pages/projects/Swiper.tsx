bec2699b75a1f0df972ece090c20096dfe0dc2fb
jihyeon <jihyeon33@gmail.com>
2023-07-07 18:52:22 +0900

    router 추가

--- src/pages/projects/Swiper.tsx
+++ src/pages/projects/Swiper.tsx
@@ -6,7 +6,7 @@ import "swiper/swiper.min.css";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "@/styles/project.module.scss";
-
+import { useRouter } from "next/router";
 /* 프로젝트 이미지 =========================================================== */
 import project01 from "@/public/images/project01.jpg";
 import project02 from "@/public/images/project02.jpg";
@@ -22,6 +22,7 @@ import project10 from "@/public/images/project10.jpg";
 /* Prop =========================================================== */
 type Prop = {};
 type ProjectProp = {
+  id: number | string;
   image: any;
   title: string;
   date: any;
@@ -32,6 +33,7 @@ export default function ProjectSlide({}) {
   SwiperCore.use([Navigation, Pagination, Autoplay]);
   const projectArray: ProjectProp[] = [
     {
+      id: 1,
       image: (
         <Image src={project01} alt="오십견각도계" width={350} height={450} />
       ),
@@ -40,6 +42,7 @@ export default function ProjectSlide({}) {
       text: <div>UX/UI 디자인 및 관리자 페이지 퍼블리싱</div>,
     },
     {
+      id: 2,
       image: (
         <Image src={project02} alt="솔루션페이지" width={350} height={450} />
       ),
@@ -48,12 +51,14 @@ export default function ProjectSlide({}) {
       text: <div>회사 솔루션 페이지 퍼블리싱</div>,
     },
     {
+      id: 3,
       image: <Image src={project03} alt="기술장인" width={350} height={450} />,
       title: "기술장인",
       date: <>2021. 01</>,
       text: <div>UX/UI 디자인 및 관리자 페이지 퍼블리싱</div>,
     },
     {
+      id: 4,
       image: (
         <Image src={project04} alt="건물임대관리" width={350} height={450} />
       ),
@@ -62,6 +67,7 @@ export default function ProjectSlide({}) {
       text: <div>UX/UI 디자인 및 웹앱, 관리자 페이지 퍼블리싱</div>,
     },
     {
+      id: 5,
       image: (
         <Image src={project05} alt="장보고배송" width={350} height={450} />
       ),
@@ -70,6 +76,7 @@ export default function ProjectSlide({}) {
       text: <div>UX/UI 디자인 및 관리자 페이지 퍼블리싱</div>,
     },
     {
+      id: 6,
       image: (
         <Image src={project06} alt="동네의영웅" width={350} height={450} />
       ),
@@ -78,12 +85,14 @@ export default function ProjectSlide({}) {
       text: <div>웹앱, 관리자 페이지 퍼블리싱</div>,
     },
     {
+      id: 7,
       image: <Image src={project07} alt="홀리데이" width={350} height={450} />,
       title: "홀리데이",
       date: <>2021. 05</>,
       text: <div>UX/UI 디자인, 관리자 페이지 퍼블리싱</div>,
     },
     {
+      id: 8,
       image: <Image src={project08} alt="씨플러스" width={350} height={450} />,
       title: "씨플러스",
       date: <>2021. 08</>,
@@ -94,6 +103,7 @@ export default function ProjectSlide({}) {
       ),
     },
     {
+      id: 9,
       image: (
         <Image src={project09} alt="체질의민족" width={350} height={450} />
       ),
@@ -102,6 +112,7 @@ export default function ProjectSlide({}) {
       text: <div>관리자 페이지 퍼블리싱</div>,
     },
     {
+      id: 10,
       image: (
         <Image src={project10} alt="모델에이전시" width={350} height={450} />
       ),
@@ -111,10 +122,13 @@ export default function ProjectSlide({}) {
     },
   ];
   const renderProjectAraay = (item: ProjectProp, index: number) => {
+    const router = useRouter();
     return (
       <SwiperSlide key={index} className="swiper_slide">
         <div className={styles.projects_wrap}>
-          <div className={styles.image}>{item.image}</div>
+          <div className={styles.image}>
+            <Link href={{ pathname: "/view/${item.id}" }}>{item.image}</Link>
+          </div>
           <div className={styles.content}>
             <div className={styles.title}>{item.title}</div>
             <div className={styles.date}>{item.date}</div>
