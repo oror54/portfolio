8353b61a257b7e7dca30f5a272638c65d48105d8
jihyeon <jihyeon33@gmail.com>
2023-08-29 21:55:59 +0900

    og내용추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,13 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta property="og:title" content="홍지현 포트폴리오" />
+        <meta property="og:url" content="https://jihyeon.vercel.app/" />
+        <meta property="og:image" content="@/public/jihyeon_portfolio.png" />
+        <meta
+          property="og:description"
+          content="웹디자이너, 웹퍼블리셔 홍지현 포트폴리오입니다"
+        />
         <meta
           name="viewport"
           content="user-scalable=no,width=device-width,initial-scale=1"
